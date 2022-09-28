61900c5cf032131d69ee04520abdee2e4c6858f8
Rahul-Singh-Dhek <rahuldhek21@gmail.com>
2022-09-28 19:30:33 +0530

    DRD

--- src/controllers/awsFunction.js
+++ src/controllers/awsFunction.js
@@ -1,34 +1,19 @@
 const aws = require('aws-sdk')
 
 
-// aws.config.update({
-//     accessKeyId: "AKIAY3L35MCRZNIRGT6N",
-//     secretAccessKeyId: "9f+YFBVcSjZWM6DG9R4TUN8k8TGe4X+lXmO4jPiU",
-//     region: "ap-south-1"
-// })
-
-// aws.config.update({
-//     region: "ap-south-1",
-//     cre
-//     accessKeyId: "AKIAY3L35MCRZNIRGT6N",
-//     secretAccessKeyId: "YFBVcSjZWM6DG9R4TUN8k8TGe4X+lXmO4jPiU",
-//     region: "ap-south-1"
-// })
-
 aws.config.update({
-    region: 'ap-south-1',
-    apiVersion: 'latest',
-    credentials: {
-      accessKeyId: 'AKIAY3L35MCRZNIRGT6N',
-      secretAccessKey: '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'
-    }
-  })
+
+    accessKeyId: 'AKIAY3L35MCRZNIRGT6N',
+    secretAccessKey: '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',
+    region: 'ap-south-1'
+
+})
 
 
-  //     Access key ID
-    // AKIAY3L35MCRZNIRGT6N
-    // Secret access key
-    // 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
+//     Access key ID
+// AKIAY3L35MCRZNIRGT6N
+// Secret access key
+// 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
 
 const uploadFile = async function (file) {
     console.log(file);
